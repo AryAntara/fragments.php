6c6e64e882674ff5279563e6332d5829402fcc50
I Komang Ary Antara <xinjiangcrosin@gmail.com>
2026-01-06 12:47:54 +0800

    feat: add ExtensionFragment class and update README with project structure and todos

---
+++ Fragments/Parts/ExtensionFragment.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace Fragments\Parts;
+
+class ExtensionFragment
+{
+    public function getName(): string
+    {
+        return 'extension';
+    }
+
+    public function getVersion(): string
+    {
+        return '1.0.0';
+    }
+}
